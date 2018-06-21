c63922f4326b50aaf59cc28c9a21e89ecb5b0838
yate <e_yangting@qq.com>
2018-06-21 14:36:48 +0800

    dev env add
    Signed-off-by: yate <e_yangting@qq.com>

--- gateway-server/src/main/java/one/yate/spring/cloud/gateway/config/ZuulConfig.java
+++ gateway-server/src/main/java/one/yate/spring/cloud/gateway/config/ZuulConfig.java
@@ -5,11 +5,25 @@ import one.yate.spring.cloud.gateway.filter.ErrorRequestLogFilter;
 import one.yate.spring.cloud.gateway.filter.PostRequestLogFilter;
 import one.yate.spring.cloud.gateway.filter.PreRequestLogFilter;
 import one.yate.spring.cloud.gateway.filter.RouteRequestLogFilter;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.cloud.netflix.zuul.filters.discovery.DiscoveryClientRouteLocator;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.cloud.netflix.zuul.filters.discovery.ServiceRouteMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ZuulConfig {
+    @Bean
+    public DiscoveryClientRouteLocator discoveryClientRouteLocator(DiscoveryClient discovery, ServerProperties server, ZuulProperties zuulProperties, ServiceInstance localServiceInstance) {
+        ServiceRouteMapper serviceRouteMapper = new PatternServiceRouteMapper("(?<name>.*)-(?<type>.*)-(?<version>v.*$)", "${name}/${version}");
+        DiscoveryClientRouteLocator routeLocator = new DiscoveryClientRouteLocator(server.getServlet().getServletPrefix(), discovery, zuulProperties, serviceRouteMapper, localServiceInstance);
+        return routeLocator;
+    }
+
     @Bean
     public ZuulFilter preRequestLogFilter() {
         return new PreRequestLogFilter();
